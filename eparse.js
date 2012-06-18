b12b86471d2fb5b73c6125db6754642779a8cb26
Pawel Marczewski <humpolec@gmail.com>
2012-06-18 15:33:48 +0200

    tokenizer, understands only numbers so far

--- eparse.js
+++ eparse.js
@@ -1,6 +1,36 @@
 
 eparse = {};
 
+// Split the string into tokens. A token is a object with keys
+// 'type', 'val'.
+eparse.tokenize = function(str) {
+    // We work by deleting from the front of the input.
+    var input = str;
+
+    // Delete spaces from the beginning of the input
+    var deleteSpaces = function() {
+        var pos = 0;
+        while (/\s/.test(input[pos]) && pos < input.length)
+            pos++;
+        input = input.substr(pos);
+    };
+
+    var tokens = [];
+    deleteSpaces();
+
+    for (; input.length > 0; deleteSpaces()) {
+        // First, look for a number
+        var m = /^\d+/.exec(input);
+        if (m != null) {
+            input = input.substr(m[0].length);
+            tokens.push({type: 'number', val: parseInt(m[0])});
+            continue;
+        }
+        throw 'lexer error';
+    }
+    return tokens;
+};
+
 // Initialize interface: terminal, operator textarea, load button
 // (given as jQuery objects)
 eparse.interface = function(terminal, ops, load) {
